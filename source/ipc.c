969b140c0c5594193ba4aee210a3919d6b44ff4e
adam majerik <adamkooo2@gmail.com>
2025-01-06 15:06:07 +0100

    added ipc, game logic and utils classes

---
+++ source/ipc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//IPC: Inter-Process Communication
+
+// Init server
+int initialize_server() {
+    //TODO Adam: imlement
+}
+
+// Send or receive messages
+void communicate() {
+    //TODO Adam: implement
+}
+
+// Main
+int main() {
+    //TODO Adam: implement
+    return 0;
+}
