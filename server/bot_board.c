b819a9dc924a471c3cd39744d9d8017f7e756df6
WaffleMaker9000 <tomkrnt@gmail.com>
2025-01-12 04:56:46 +0100

    Random generation of board

--- server/bot_board.c
+++ server/bot_board.c
@@ -3,8 +3,21 @@
 void generate_board(board* b)
 {
     int placed = 0;
+    int size = 5;
+    char* position = malloc(3 * sizeof(char));
     while(placed < 5)
     {
-        
+        position[0] = 'A' + rand() % b->size_;
+        position[1] = '0' + rand() % b->size_;
+        position[2] = rand() % 2 ? 'd' : 'r';
+        if (validate_position(position, size, b))
+        {
+            finalise_placement(position, size, b);
+            placed++;
+            if (placed != 3)
+            {
+                size--;
+            }
+        }
     }
 }
